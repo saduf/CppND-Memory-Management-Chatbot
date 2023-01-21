d510c614d4a6c7a6c85e3ae2aa1be13d4b3bc051
saduf <fernandosadu@gmail.com>
2023-01-21 08:54:12 -0500

    allocating memory in copy constructor/assignment operator

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -47,6 +47,7 @@ ChatBot::~ChatBot()
 ChatBot::ChatBot(const ChatBot &source) // copy constructor
 {
     std::cout << "ChatBot Copy Constructor" << std::endl;
+    _image = new wxBitmap();
     _image = source._image;
     _currentNode = source._currentNode;
     _chatLogic = source._chatLogic;
@@ -60,6 +61,7 @@ ChatBot &ChatBot::operator=(const ChatBot &source)
     if (this == &source)
         return *this;
     delete _image;
+    _image = new wxBitmap();
     _image = source._image;
     _currentNode = source._currentNode;
     _chatLogic = source._chatLogic;
@@ -78,7 +80,6 @@ ChatBot::ChatBot(ChatBot &&source) // move constructor
     source._currentNode = nullptr;
     source._chatLogic = nullptr;
     source._rootNode = nullptr;
-    // _chatLogic->SetChatbotHandle(this);
 }
 
 ChatBot &ChatBot::operator=(ChatBot &&source) // move assignment operator
